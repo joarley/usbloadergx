f900a70ccebeb95ff6d9306ca52b7351f9b85bbc
Joarley Santos <joarley.santos@gmail.com>
2016-02-19 15:46:16 -0200

    Debug client dev

--- debug-client-win/Form1.Designer.cs
+++ debug-client-win/Form1.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(627, 303);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(331, 339);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // start
+            // 
+            this.start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.start.Location = new System.Drawing.Point(483, 337);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 2;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.stop.Location = new System.Drawing.Point(564, 337);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 3;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 342);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Clear Logs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(153, 337);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(68, 23);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Auto Scroll";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 372);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- debug-client-win/Form1.cs
+++ debug-client-win/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,156 @@ namespace DebugOutput
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            start.Enabled = true;
+            stop.Enabled = false;
+            textBox1.Enabled = true;
+
+            Task.Run(Main);
+        }
+
+        enum ParseState
+        {
+            NotInitilized,
+            Initialized,
+            IdFound,
+            MessageFound,
+            EndFound
+        }
+
+        private Task Main()
+        {
+            TcpClient client;
+
+            while (true)
+            {
+                Invoke(new Action(() => { label1.Text = "Desconnected"; label1.ForeColor = Color.Red; }));
+
+                if (start.Enabled)
+                {
+                    Thread.Sleep(500);
+                    continue;
+                }
+
+                try
+                {
+                    var ip = textBox1.Text.Split(':')[0];
+                    var port = int.Parse(textBox1.Text.Split(':')[1]);
+                    client = new TcpClient();
+                    var taskConnect = client.ConnectAsync(ip, port);
+                    taskConnect.Wait(2000);
+                    if (!client.Connected)
+                    {
+                        client.Close();
+                        continue;
+                    }
+
+                    Invoke(new Action(() => { label1.Text = "Connected"; label1.ForeColor = Color.Green; }));
+
+                    List<string> workingPieces = new List<string>();
+                    ParseState state = ParseState.NotInitilized;
+
+                    while (stop.Enabled)
+                    {
+                        var stream = client.GetStream();
+
+                        if (!stream.DataAvailable)
+                        {
+                            Thread.Sleep(300);
+                            continue;
+                        }
+
+                        byte[] buffer = new byte[5000];
+
+                        stream.Read(buffer, 0, buffer.Length);
+
+                        var dataReceived = Encoding.UTF8.GetString(buffer);
+                        var piecesReceived = dataReceived.Split(new[] { "%&_" }, StringSplitOptions.None);
+
+                        if (!piecesReceived.Any())
+                        {
+                            Thread.Sleep(300);
+                            continue;
+                        }
+
+                        foreach (var piece in piecesReceived)
+                        {
+                            if (piece == "INIT")
+                            {
+                                if (state != ParseState.NotInitilized)
+                                    workingPieces.Clear();
+                                workingPieces.Add(piece);
+                                state = ParseState.Initialized;
+                            }
+                            else if (piece == "END")
+                            {
+                                state = ParseState.NotInitilized;
+
+                                if (state == ParseState.MessageFound)
+                                {
+                                    workingPieces.Add(piece);
+                                    ProcessMessage(workingPieces.ToArray());
+                                }
+
+                                workingPieces.Clear();
+                            }
+                            else if (state == ParseState.Initialized)
+                            {
+                                workingPieces.Add(piece);
+                                state = ParseState.IdFound;
+                            }
+                            else if (state == ParseState.IdFound)
+                            {
+                                workingPieces.Add(piece);
+                                state = ParseState.MessageFound;
+                            }
+                            else if (state == ParseState.MessageFound)
+                            {
+                                workingPieces[2] = workingPieces[2] + piece;
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(500);
+                    continue;
+                }
+            }
+        }
+
+        private void ProcessMessage(string[] dataPieces)
+        {
+            string idMessage = dataPieces[1];
+            string workingMessage = dataPieces[2];
+
+            Invoke(new Action(()=> {
+                listBox1.Items.Add(idMessage + "- " + workingMessage);
+                if (checkBox1.Checked)
+                    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            }));
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            start.Enabled = true;
+            stop.Enabled = false;
+            textBox1.Enabled = true;
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            start.Enabled = false;
+            stop.Enabled = true;
+            textBox1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
         }
     }
 }
